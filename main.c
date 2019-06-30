b69b5cf072ff4adcd425bf882f7f6e6d7a2a290a
shaghayegh <shaghayegh5ghasemi@gmail.com>
2019-06-30 15:41:11 +0430

    bug func print_task fixed

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ void add_front(struct program **plist, struct program *new_program){
     new_program->next = *plist;
     *plist = new_program;
 }
-void add_end(struct program *list, struct program new_program){
+void add_end(struct program *list, struct program *new_program){
     struct program *current;
     for(current  = list;current->next != NULL; current = current->next);
     current->next = new_program;
@@ -78,56 +78,58 @@ struct program* new_task(){
 }
 void print_tasks(struct program *list){
     struct program *current;
-    printf("0 (Saturday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("0 (Saturday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 0){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
+
         }
     }
+
     printf("\n");
-    printf("1 (Sunday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("1 (Sunday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 1){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
         }
     }
     printf("\n");
-    printf("2 (Monday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("2 (Monday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 2){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
         }
     }
     printf("\n");
-    printf("3 (Tuesday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("3 (Tuesday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 3){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
         }
     }
     printf("\n");
-    printf("4 (Wednesday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("4 (Wednesday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 4){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
         }
     }
     printf("\n");
-    printf("5 (Thursday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("5 (Thursday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 5){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
         }
     }
     printf("\n");
-    printf("6 (Friday): ");
-    for(current = list; current != NULL; current = current->name){
+    printf("6 (Friday): \n");
+    for(current = list; current != NULL; current = current->next){
         if(current->day == 6){
             printf("%s (from: %d, to: %d)\n", current->name
                    , current->start, current->end);
@@ -143,38 +145,33 @@ int main()
     struct program *list = NULL;
     printf("Enter your name: ");
     scanf("%s", name);
-    printf("Welcome %s!", name);
-    options();
-    scanf("%d", &choice);
+    printf("Welcome %s!\n", name);
     struct program *nt;
     FILE *fp;
-    while(choice != 5){
+    while(1){
+        options();
+        scanf("%d", &choice);
         switch(choice){
-        case 1:
-            nt = new_task();
-            if(nt == NULL)
-                printf("there is no space to add your task!");
-            else
-                insert_task(&(list), nt);
-
-        case 2:
-            print_tasks(list);
-        case 3:
-            fp = fopen(name, "w");
-            if(fp == NULL)
-                printf("Can not open a file to save your data!");
-            else{
-                fprint_tasks(list, fp);
-            }
-            fclose(fp);
-        case 4:
-            fopen(name, "r+");
-            //??
-    }
-    if(choice == 5){
-        printf("Bye %s", name);
-        return 0;
-    }
+            case 1:
+                nt = new_task();
+                if(nt == NULL)
+                    printf("there is no space to add your task!");
+                else
+                    insert_task(&(list), nt);
+                break;
+            case 2:
+                print_tasks(list);
+                break;
+            case 3:
+                continue;
+                break;
+            case 4:
+                continue;
+                break;
+            case 5:
+                printf("Bye %s", name);
+                return 0;
+        }
     }
     return 0;
 }
